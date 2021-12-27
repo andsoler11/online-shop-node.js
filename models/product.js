feca03031a1b338dbe8f0cf8fda2e2103ed1e37a
Andres Soler <andres.soler@talent.com>
2021-12-27 11:22:13 -0500

    added cart functions and edit product function

--- models/product.js
+++ models/product.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
+const { createRequire } = require('module');
 const path = require('path');
+const Cart = require('./cart');
 
 const p = path.join(
     path.dirname(require.main.filename), 
@@ -20,7 +22,8 @@ const getProductsFromFile = (callback) => {
 
 
 module.exports = class Product {
-    constructor(title, imgUrl, price, description) {
+    constructor(id, title, imgUrl, price, description) {
+        this.id = id;
         this.title = title;
         this.imgUrl = imgUrl;
         this.price = price;
@@ -28,14 +31,38 @@ module.exports = class Product {
     }
 
     save() {
-        this.id = Math.random().toString();
         getProductsFromFile(products => {
-            products.push(this);
-            fs.writeFile(p, JSON.stringify(products), (err) => {
-                console.log(err);
-            });
+            if(this.id) {
+                const existingProductIndex = products.findIndex(prod => prod.id === this.id)
+                const updatedProducts = [...products];
+                updatedProducts[existingProductIndex] = this
+                fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+                    console.log(err);
+                });
+            } else {
+                this.id = Math.random().toString();
+                products.push(this);
+                fs.writeFile(p, JSON.stringify(products), (err) => {
+                    console.log(err);
+                });
+            }
         }); 
     }
+
+    static deleteById(id) {
+        getProductsFromFile(products => {
+            const product = products.find(prod => prod.id === id);
+            const updatedProducts = products.filter(p => p.id !== id);
+            fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+                if(!err){
+                    Cart.deleteProduct(id, product.price);
+                }
+            })
+            // callback(product);
+        })
+    }
+
+
 
     static fetchAll(callback) {
         getProductsFromFile(callback);
