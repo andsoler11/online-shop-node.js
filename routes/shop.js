0b2058c49a7dc5b7003e0a41d403740f304df32c
Andres Soler <andres.soler@talent.com>
2021-12-27 00:10:15 -0500

    first commit

---
+++ routes/shop.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const express = require('express');
+const router = express.Router();
+const shopController = require('../controllers/shop');
+
+
+router.get('/', shopController.getIndex);
+router.get('/products', shopController.getProducts);
+// router.get('/products/delete', shopController.getOrders);
+// /products/12345
+router.get('/products/:productId', shopController.getProduct);
+
+router.get('/cart', shopController.getCart);
+router.post('/cart', shopController.postCart);
+
+
+router.get('/checkout', shopController.getCheckout);
+router.get('/orders', shopController.getOrders);
+
+
+module.exports = router;
